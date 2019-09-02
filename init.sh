7f3490c9ef0fdbba9f5ed71561d1321b455c6633
Ubuntu <lik@infolik.iy141fyf3xuezokvotxop01fsf.bx.internal.cloudapp.net>
2019-09-02 08:36:53 +0000

    add initiation script for infersent encoder

---
+++ init.sh
@@ -0,0 +1,12 @@
+#/bin/bash
+mkdir GloVe
+curl -Lo GloVe/glove.840B.300d.zip http://nlp.stanford.edu/data/glove.840B.300d.zip
+unzip GloVe/glove.840B.300d.zip -d GloVe/
+mkdir fastText
+curl -Lo fastText/crawl-300d-2M.vec.zip https://dl.fbaipublicfiles.com/fasttext/vectors-english/crawl-300d-2M.vec.zip
+unzip fastText/crawl-300d-2M.vec.zip -d fastText/
+mkdir encoder
+curl -Lo encoder/infersent1.pkl https://dl.fbaipublicfiles.com/infersent/infersent1.pkl
+curl -Lo encoder/infersent2.pkl https://dl.fbaipublicfiles.com/infersent/infersent2.pkl
+python -m nltk.downloader punkt
+git clone https://github.com/facebookresearch/InferSent.git
