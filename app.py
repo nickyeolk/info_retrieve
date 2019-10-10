f7d2e1068712a175d109568f97a907cf1d9c666d
likkhian <likkhian@hotmail.com>
2019-10-10 06:54:18 +0000

    update api

--- app.py
+++ app.py
@@ -7,10 +7,12 @@ gr = GoldenRetriever()
 gr.restore('/data/home/lik/info_retrieve/google_use_nrf_tuned/variables-0')
 gr.load_kb(path_to_kb='./data/aiap.txt', is_faq=True)
 
-@app.route('/', methods=['GET'])
+@app.route('/', methods=['POST', 'GET'])
 def get_prediction():
-    data = request.form.get("question")
+    # data = request.form.get("question")
+    data=request.args.get("question")
     reply, _ = gr.make_query(data, top_k=1)
+    print('request received!')
     return reply[0]
 
 if __name__ == '__main__':
