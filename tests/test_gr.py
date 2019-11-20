7f32e507e8bcf0bb3f6df0bc61abaede4335dfcb
Lik Khian Yeo <t0916975@kelaberetiv.sg>
2019-11-20 10:43:35 +0800

    debug test

--- tests/test_gr.py
+++ tests/test_gr.py
@@ -1,6 +1,6 @@
 # import sys
 # sys.path.append('../..')
-from GoldenRetriever.src.utils import clean_txt
+from src.utils import clean_txt
 
 def test_clean_txt():
     assert clean_txt(['this\nand that.. '])==['this. and that.']
