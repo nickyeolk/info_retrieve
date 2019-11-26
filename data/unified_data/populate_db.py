888c2a204747d3589f237e1c1e6a26c5bdde6087
likkhian <likkhian@hotmail.com>
2019-11-26 11:34:46 +0000

    create database

---
+++ data/unified_data/populate_db.py
@@ -0,0 +1,10 @@
+from create_db import create_connection
+
+def create_kb_raw(conn, kb_raw)
+
+def main():
+    conn = create_connection('./pythonsqlite.db')
+
+
+if __name__ == '__main__':
+    main()
