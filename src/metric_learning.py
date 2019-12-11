7e7e32f5f996055309b1b59cc114ceb16783f937
vinitrinh <jeremy_tan@kelaberetiv.sg>
2019-12-11 17:38:17 +0800

    TF2 Class main functions

--- src/metric_learning.py
+++ src/metric_learning.py
@@ -1,8 +1,5 @@
-# from tensorflow.python.ops import array_ops
-from tensorflow.losses import cosine_distance
 import tensorflow as tf
-# from tensorflow.python.ops import math_ops
-from tensorflow.python.framework import dtypes
+
 
 def triplet_loss(anchor_vector, positive_vector, negative_vector, metric='cosine_dist', margin=0.009):
     """Computes the triplet loss with semi-hard negative mining.
@@ -24,39 +21,10 @@ def triplet_loss(anchor_vector, positive_vector, negative_vector, metric='cosine
     Returns:
         triplet_loss: tf.float32 scalar.
     """
-    d_pos = cosine_distance(anchor_vector, positive_vector, axis=1)
-    d_neg = cosine_distance(anchor_vector, negative_vector, axis=1)
+    cosine_distance = tf.keras.losses.CosineSimilarity(axis=1)
+    d_pos = cosine_distance(anchor_vector, positive_vector)
+    d_neg = cosine_distance(anchor_vector, negative_vector)
     loss = tf.maximum(0., margin + d_pos - d_neg)
     loss = tf.reduce_mean(loss)
     return loss
 
-
-def contrastive_loss(labels, embeddings_anchor, embeddings_positive,
-                     margin=1.0):
-  """Computes the contrastive loss.
-  This loss encourages the embedding to be close to each other for
-  the samples of the same label and the embedding to be far apart at least
-  by the margin constant for the samples of different labels.
-  See: http://yann.lecun.com/exdb/publis/pdf/hadsell-chopra-lecun-06.pdf
-
-  Args:
-      labels: 1-D tf.int32 `Tensor` with shape [batch_size] of
-      binary labels indicating positive vs negative pair.
-      embeddings_anchor: 2-D float `Tensor` of embedding vectors for the anchor
-      images. Embeddings should be l2 normalized.
-      embeddings_positive: 2-D float `Tensor` of embedding vectors for the
-      positive images. Embeddings should be l2 normalized.
-      margin: margin term in the loss definition.
-
-  Returns:
-      contrastive_loss: tf.float32 scalar.
-  """
-  # Get per pair distances
-  distances = cosine_distance(embeddings_anchor, embeddings_positive, axis=1)
-  # Add contrastive loss for the siamese network.
-  #   label here is {0,1} for neg, pos.
-  return tf.reduce_mean(
-      tf.cast(labels, dtypes.float32) * distances +
-      (1. - tf.cast(labels, dtypes.float32)) *
-      tf.maximum(margin - distances, 0.),
-      name='contrastive_loss')
--- src/model.py
+++ src/model.py
@@ -1,11 +1,15 @@
 import tensorflow as tf
+import tensorflow_addons as tfa
 import tensorflow_hub as hub
 import numpy as np
-import tf_sentencepiece
-from .metric_learning import triplet_loss, contrastive_loss
-from tensorflow.train import Saver
+import datetime
+import tensorflow_text
 from .utils import split_txt, read_txt, clean_txt, read_kb_csv
 from sklearn.metrics.pairwise import cosine_similarity
+from tensorflow.keras.optimizers import Adam
+
+from .metric_learning import triplet_loss
+
 
 class GoldenRetriever:
     """GoldenRetriever model for information retrieval prediction and finetuning.
@@ -23,110 +27,109 @@ class GoldenRetriever:
     ['I hate Mondays.']
     """
     
-    def __init__(self, lr=0.6, margin=0.3, loss='triplet'):
-        # self.v=['module/QA/Final/Response_tuning/ResidualHidden_1/dense/kernel','module/QA/Final/Response_tuning/ResidualHidden_0/dense/kernel', 'module/QA/Final/Response_tuning/ResidualHidden_1/AdjustDepth/projection/kernel']
-        self.v=['module/QA/Final/Response_tuning/ResidualHidden_1/AdjustDepth/projection/kernel']
+    
+    def __init__(self, lr=0.6, margin=0.3, loss='cosine'):
+        # self.v=['QA/Final/Response_tuning/ResidualHidden_1/dense/kernel','QA/Final/Response_tuning/ResidualHidden_0/dense/kernel', 'QA/Final/Response_tuning/ResidualHidden_1/AdjustDepth/projection/kernel']
+        self.v=['QA/Final/Response_tuning/ResidualHidden_1/dense/kernel']
         self.lr = lr
         self.margin = margin
         self.loss = loss
         self.vectorized_knowledge = {}
         self.text = {}
         self.questions = {}
-        # Set up graph.
-        tf.reset_default_graph() # finetune
-        g = tf.Graph()
-        with g.as_default():
-            # self.embed = hub.Module("./google_use_qa", trainable=True)
-            self.embed = hub.Module("https://tfhub.dev/google/universal-sentence-encoder-multilingual-qa/1", trainable=True)
-            # put placeholders
-            self.question = tf.placeholder(dtype=tf.string, shape=[None])  # question
-            self.response = tf.placeholder(dtype=tf.string, shape=[None])  # response
-            self.response_context = tf.placeholder(dtype=tf.string, shape=[None])  # response context
-            self.neg_response = tf.placeholder(dtype=tf.string, shape=[None])  # response
-            self.neg_response_context = tf.placeholder(dtype=tf.string, shape=[None])  # response context
-            self.label = tf.placeholder(tf.int32, [None], name='label')
-            
-            self.question_embeddings = self.embed(
-            dict(input=self.question),
-            signature="question_encoder", as_dict=True)
 
-            self.response_embeddings = self.embed(
-            dict(input=self.response,
-                context=self.response_context),
-            signature="response_encoder", as_dict=True)
-
-            self.neg_response_embeddings = self.embed(
-            dict(input=self.neg_response,
-                context=self.neg_response_context),
-            signature="response_encoder", as_dict=True)
-
-            init_op = tf.group([tf.global_variables_initializer(), tf.tables_initializer()])
-
-            # finetune
-            # tf 1.13 does not have contrastive loss. Might have to self-implement.
-            if self.loss=='triplet':
-                self.cost = triplet_loss(self.question_embeddings['outputs'], self.response_embeddings['outputs'], self.neg_response_embeddings['outputs'], margin=self.margin)
-            elif self.loss=='cosine':
-                self.cost = tf.losses.cosine_distance(self.question_embeddings['outputs'], self.response_embeddings['outputs'], axis=1)
-            elif self.loss=='contrastive':
-                self.cost = contrastive_loss(self.label, self.question_embeddings['outputs'], self.response_embeddings['outputs'], margin=self.margin)
-            else: raise NotImplementedError('invalid loss selected. Choose either triplet or cosine.')
-            opt = tf.train.GradientDescentOptimizer(learning_rate=self.lr)
-            var_finetune=[x for x in self.embed.variables for vv in self.v if vv in x.name] #get the weights we want to finetune.
-            self.opt_op = opt.minimize(self.cost, var_list=var_finetune)
-        # g.finalize()
-
-        # Initialize session.
-        self.session = tf.Session(graph=g, config=tf.ConfigProto(log_device_placement=False))
-        self.session.run(init_op)
+        # init saved model
+        self.embed = hub.load('https://tfhub.dev/google/universal-sentence-encoder-multilingual-qa/2')
+        self.question_encoder = self.embed.signatures['question_encoder']
+        self.response_encoder = self.embed.signatures['response_encoder']
+        self.neg_response_encoder = self.embed.signatures['response_encoder']
         print('model initiated!')
-    
+        
+        # optimizer & losses
+        self.optimizer = tf.keras.optimizers.Adam()
+        self.cost_history = []
+        
+
+        
+        
     def predict(self, text, context=None, type='response'):
         """Return the tensor representing embedding of input text.
         Type can be 'query' or 'response' """
         if type=='query':
-            return self.session.run(self.question_embeddings, feed_dict={self.question:text})['outputs']
+            return self.question_encoder(tf.constant([text]))['outputs']
+            # return self.session.run(self.question_embeddings, feed_dict={self.question:text})['outputs']
         elif type=='response':
             if not context:
                 context = text
-            return self.session.run(self.response_embeddings, feed_dict={
-            self.response:text,
-            self.response_context:context
-            })['outputs']
+            return self.response_encoder(input=tf.constant(text),
+                                         context=tf.constant(context))['outputs']
         else: print('Type of prediction not defined')
-
-    def finetune(self, question, answer, context, neg_answer=[], neg_answer_context=[], label=[]):
-        current_loss = self.session.run(self.cost, feed_dict={
-            self.question:question,
-            self.response:answer,
-            self.response_context:context,
-            self.neg_response:neg_answer,
-            self.neg_response_context:neg_answer_context,
-            self.label:label
-            })
-        self.session.run(self.opt_op, feed_dict={
-            self.question:question,
-            self.response:answer,
-            self.response_context:context,
-            self.neg_response:neg_answer,
-            self.neg_response_context:neg_answer_context,
-            self.label:label
-            })
-        return current_loss
         
-    def export(self, savepath='fine_tuned', step=0):
-        '''Path should include partial filename.'''
-        saver=Saver(self.embed.variables)
-        saver.save(self.session, savepath, global_step=step)
-    
-    def restore(self, savepath):
-        saver=Saver(self.embed.variables)
-        saver.restore(self.session, savepath)
-        print('model checkpoint restored!')
+    def make_query(self, querystring, top_k=5, index=False, predict_type='query', kb_name='default_kb'):
+        """Make a query against the stored vectorized knowledge. 
+        Choose index=True to return sorted index of matches.
+        type can be 'query' or 'response' if you are comparing statements
+        """
+        similarity_score=cosine_similarity(self.vectorized_knowledge[kb_name], self.predict([querystring], type=predict_type))
+        sortargs=np.flip(similarity_score.argsort(axis=0))
+        sortargs=[x[0] for x in sortargs]
+        sorted_ans=[self.text[kb_name][i] for i in sortargs]
+        if index:
+            return sorted_ans[:top_k], sortargs[:top_k]
+        return sorted_ans[:top_k], similarity_score[sortargs[:top_k]] 
+        
+        
+    def finetune(self, question, answer, context, neg_answer=[], neg_answer_context=[], label=[]):
+        """
+        Apply gradients on
+        """
+        with tf.GradientTape() as tape:
+            # https://www.tensorflow.org/guide/eager
 
-    def close(self):
-        self.session.close()
+            # get encodings
+            question_embeddings = self.question_encoder(tf.constant(question))['outputs']
+            response_embeddings = self.response_encoder(input=tf.constant(answer), 
+                                                        context=tf.constant(context))['outputs']
 
+            if self.loss == 'cosine':
+                """
+                # https://www.tensorflow.org/api_docs/python/tf/keras/losses/CosineSimilarity
+                """
+                self.cost = tf.keras.losses.CosineSimilarity(axis=1)
+                cost_value = self.cost(question_embeddings, response_embeddings)
+                
+            elif self.loss == 'contrastive':
+                """
+                https://www.tensorflow.org/addons/api_docs/python/tfa/losses/ContrastiveLoss
+                
+                y_true to be a vector of binary labels
+                y_hat to be the respective distances
+                """
+                self.cosine_dist = tf.keras.losses.CosineSimilarity(axis=1)
+                cosine_dist_value = self.cosine_dist(question_embeddings, response_embeddings)
+                
+                self.cost = tfa.losses.contrastive.ContrastiveLoss(margin = self.margin)
+                cost_value = self.cost(label, cosine_dist_value)
+                
+            elif self.loss == 'triplet':
+                """
+                https://www.tensorflow.org/addons/tutorials/losses_triplet
+                """
+                neg_response_embeddings = self.neg_response_encoder(input=tf.constant(neg_answer), 
+                                                                    context=tf.constant(neg_answer_context))['outputs']
+                self.cost = tfa.losses.TripletSemiHardLoss(margin = self.margin)
+                cost_value = triplet_loss(question_embeddings, response_embeddings, neg_response_embeddings)
+
+                
+        # record loss     
+        self.cost_history.append(cost_value.numpy().mean())
+        
+        # apply gradient
+        grads = tape.gradient(cost_value, var_finetune)
+        optimizer.apply_gradients(zip(grads, var_finetune))
+
+        return cost_value.numpy().mean()
+        
     def load_kb(self, path_to_kb=None, text_list=None, question_list=None, 
                 raw_text=None, is_faq=False, kb_name='default_kb'):
         r"""Give either path to .txt document or list of clauses.
@@ -146,71 +149,66 @@ class GoldenRetriever:
         else: raise NameError('invalid kb input!')
         self.vectorized_knowledge[kb_name] = self.predict(clean_txt(self.text[kb_name]), type='response')
         print('knowledge base lock and loaded!')
-
+        
     def load_csv_kb(self, path_to_kb=None, kb_name='default_kb', meta_col='meta', answer_col='answer', 
                     query_col='question', answer_str_col='answer', cutoff=None):
         self.text[kb_name], self.questions[kb_name] = read_kb_csv(path_to_kb, meta_col=meta_col, answer_col=answer_col, 
                             query_col=query_col, answer_str_col=answer_str_col, cutoff=None)
         self.vectorized_knowledge[kb_name] = self.predict(clean_txt(self.text[kb_name]), type='response')
         print('knowledge base (csv) lock and loaded!')
+        
+    def export(self, savepath='fine_tuned_{}'.format(datetime.datetime.now())):
+        '''Path should include partial filename.'''
+        tf.saved_model.save(self.embed, 'fine_tuned')
 
-    def make_query(self, querystring, top_k=5, index=False, predict_type='query', kb_name='default_kb'):
-        """Make a query against the stored vectorized knowledge. 
-        Choose index=True to return sorted index of matches.
-        type can be 'query' or 'response' if you are comparing statements
-        """
-        similarity_score=cosine_similarity(self.vectorized_knowledge[kb_name], self.predict([querystring], type=predict_type))
-        sortargs=np.flip(similarity_score.argsort(axis=0))
-        sortargs=[x[0] for x in sortargs]
-        sorted_ans=[self.text[kb_name][i] for i in sortargs]
-        if index:
-            return sorted_ans[:top_k], sortargs[:top_k]
-        return sorted_ans[:top_k], similarity_score[sortargs[:top_k]]
+    def restore(self, savepath):
+        self.embed = tf.saved_model.load(savepath)
 
 
 
 '''Unused models below'''
-class _USEModel:
-    def __init__(self):
-        g=tf.Graph()
-        with g.as_default():
-            embed = hub.Module("./google_use")
-            self.statement = tf.placeholder(dtype=tf.string, shape=[None]) #text input
-            self.embeddings = embed(
-                dict(text=self.statement),
-                as_dict=True
-            )
-            init_op = tf.group([tf.global_variables_initializer(), tf.tables_initializer()])
-        g.finalize()
-        self.session = tf.Session(graph=g)
-        self.session.run(init_op)
+# class _USEModel:
+#     def __init__(self):
+#         g=tf.Graph()
+#         with g.as_default():
+#             embed = hub.Module("./google_use")
+#             self.statement = tf.placeholder(dtype=tf.string, shape=[None]) #text input
+#             self.embeddings = embed(
+#                 dict(text=self.statement),
+#                 as_dict=True
+#             )
+#             init_op = tf.group([tf.global_variables_initializer(), tf.tables_initializer()])
+#         g.finalize()
+#         self.session = tf.Session(graph=g)
+#         self.session.run(init_op)
         
-    def predict(self, text):
-        return self.session.run(self.embeddings, feed_dict={self.statement:text})['default']
+#     def predict(self, text):
+#         return self.session.run(self.embeddings, feed_dict={self.statement:text})['default']
 
-    def close(self):
-        self.session.close()
+#     def close(self):
+#         self.session.close()
 
-class _InferSent:
-    def __init__(self):
-        from InferSent.models import InferSent
-        import torch
-        V = 1
-        MODEL_PATH = 'encoder/infersent%s.pkl' % V
-        params_model = {'bsize': 256, 'word_emb_dim': 300, 'enc_lstm_dim': 2048,
-                        'pool_type': 'max', 'dpout_model': 0.0, 'version': V}
-        self.infersent = InferSent(params_model)
-        self.infersent.load_state_dict(torch.load(MODEL_PATH))
-        W2V_PATH = 'fastText/crawl-300d-2M.vec'
-        self.infersent.set_w2v_path(W2V_PATH)
+# class _InferSent:
+#     def __init__(self):
+#         from InferSent.models import InferSent
+#         import torch
+#         V = 1
+#         MODEL_PATH = 'encoder/infersent%s.pkl' % V
+#         params_model = {'bsize': 256, 'word_emb_dim': 300, 'enc_lstm_dim': 2048,
+#                         'pool_type': 'max', 'dpout_model': 0.0, 'version': V}
+#         self.infersent = InferSent(params_model)
+#         self.infersent.load_state_dict(torch.load(MODEL_PATH))
+#         W2V_PATH = 'fastText/crawl-300d-2M.vec'
+#         self.infersent.set_w2v_path(W2V_PATH)
     
-    def build_vocab(self, queries):
-        self.infersent.build_vocab(queries, tokenize=True)
+#     def build_vocab(self, queries):
+#         self.infersent.build_vocab(queries, tokenize=True)
     
-    def update_vocab(self, text):
-        self.infersent.update_vocab(text, tokenize=True)
+#     def update_vocab(self, text):
+#         self.infersent.update_vocab(text, tokenize=True)
 
-    def predict(self, text):
-        # self.update_vocab(text)
-        return self.infersent.encode(text, tokenize=True)
+#     def predict(self, text):
+#         # self.update_vocab(text)
+#         return self.infersent.encode(text, tokenize=True)
         
+
