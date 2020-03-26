25740ae50e7fa15c3765f2ff58f9c86f5baafc4a
kenneth_wang <ken.wangtm@gmail.com>
2020-03-27 00:33:47 +0800

    remove commented code

--- evaluate_code.sh
+++ evaluate_code.sh
@@ -1,41 +1,10 @@
 #!/bin/bash
 
-# export POLYAXON_NO_OP=1
-
-#app_name=$(git branch | grep \* | cut -d ' ' -f2)
 app_name=$CI_COMMIT_REF_NAME
-
-
-# next_assignment=$(find . -maxdepth 1 -name "assignment*" | wc -l | tr -d '[:space:]')
-
-# current=$(($next_assignment - 1))
-
-# echo "Current assignment: $current"
-
-# current_assignment="assignment$current"
-
-# echo "Evaluating $current_assignment"
-
-# post_evaluation() {
-
-#     curl -X POST \
-#       "$WEB_HOOK_URL" \
-#       -H "Content-Type: application/json; charset=UTF-8" \
-#       -d '{ "text": "'"$post_text"'",
-#            "thread": { "name": "spaces/'"${CHATROOM_ID}"'/threads/'"${!current_assignment}"'" }
-#       }'
-
-# }
-
-# echo "export PATH="~/anaconda3/bin:$PATH"" >> ~/.bashrc
-# source ~/.bashrc
 
 cat /proc/version
 echo "Updating apt-get"
 apt-get update
-
-
-# aptitude reinstall gcc-5 g++-5
 
 echo "Installing microsoft related packages"
 apt-key adv --keyserver keyserver.ubuntu.com --recv-keys EB3E94ADBE1229CF
@@ -43,11 +12,10 @@ apt-get install apt-transport-https ca-certificates
 curl https://packages.microsoft.com/keys/microsoft.asc | apt-key add
 curl https://packages.microsoft.com/config/ubuntu/16.04/prod.list > /etc/apt/sources.list.d/mssql-release.list
 
-echo "Updating apt-get"
-apt-get update
 echo "Updating libc6"
 apt-cache policy libc6
-apt-get install libc6:i386
+apt-get install libc6
+
 echo "Installing build-essential"
 apt-get -y install --reinstall build-essential
 
@@ -59,7 +27,6 @@ apt-get -y install unixodbc unixodbc-dev
 
 echo "Installing sqlalchemy"
 apt-get install -y python-sqlalchemy
-
 
 if [ -d "./tests" ]
 then
@@ -86,5 +53,4 @@ then
 else
     echo "No tests. Skipping eval and posting to chat channel."
     post_text="No tests. Skipping eval for $app_name"
-    # post_evaluation
 fi
