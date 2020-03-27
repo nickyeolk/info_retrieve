bde4e0fbbe8b7087547445c57ba9920dd7aca63a
kenneth_wang <ken.wangtm@gmail.com>
2020-03-27 18:12:55 +0800

    try building with environment.yml instead

--- evaluate_code.sh
+++ evaluate_code.sh
@@ -37,9 +37,9 @@ then
         exit 1
     fi
 
-    # conda env update -n base --file "./environment.yml"
-    pip install --upgrade pip
-    pip install -r "./requirements.txt"
+    conda env update -n base --file "./environment.yml"
+    # pip install --upgrade pip
+    # pip install -r "./requirements.txt"
     # Manually put these back since conda env update removes them
     pip install pytest pylint radon
 
