67dcb2ffbe82d35c7a8f9d0a29e792e3f9d598a7
kenneth_wang <ken.wangtm@gmail.com>
2020-03-28 18:44:16 +0800

    install certificates

--- evaluate_code.sh
+++ evaluate_code.sh
@@ -8,8 +8,8 @@ echo "Installing curl"
 apt install -y curl
 
 echo "Installing microsoft odbcsql related packages"
-# apt-key adv --keyserver keyserver.ubuntu.com --recv-keys EB3E94ADBE1229CF
-# apt-get install apt-transport-https ca-certificates
+apt-key adv --keyserver keyserver.ubuntu.com --recv-keys EB3E94ADBE1229CF
+apt-get install apt-transport-https ca-certificates
 curl https://packages.microsoft.com/keys/microsoft.asc | apt-key add
 curl https://packages.microsoft.com/config/ubuntu/16.04/prod.list > /etc/apt/sources.list.d/mssql-release.list
 
